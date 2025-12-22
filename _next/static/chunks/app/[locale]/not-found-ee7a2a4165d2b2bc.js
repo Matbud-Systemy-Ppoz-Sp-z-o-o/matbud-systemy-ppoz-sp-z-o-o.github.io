cfd3c9f6a3206a813afb443eda58d5dca09acc7f
neloduka-sobe <nelodukasobe@gmail.com>
2025-12-22 14:52:39 -0500

    Auto-deploy from Next.js build

---
+++ _next/static/chunks/app/[locale]/not-found-ee7a2a4165d2b2bc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[224,345],{4310:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=4310)),_N_E=e.O()}]);
